7faad911ba4a96aacb2a19dc81d7efc7fc5e0b37
unknown <o.daoued@gmail.com>
2024-06-06 00:39:32 +0300

    Function Exercises JS

---
+++ app.js
@@ -0,0 +1,60 @@
+// Q1
+// let x = "123456";
+// function oppsite() {
+//   return x.split("").reverse().join("");
+// }
+// console.log(oppsite());
+
+// Q2
+// function iteration() {
+//   for (let i = 0; i <= 15; i++)
+//     if (i % 2 == 0) {
+//       console.log(i + " is Even");
+//     } else {
+//       console.log(i + " is Odd");
+//     }
+// }
+// iteration();
+
+// Q3
+// let x = "Orange";
+// function alfa(x) {
+//   return x.toLowerCase().split("").sort().join("");
+// }
+// console.log(alfa(x));
+
+// Q4
+// let x = prompt("Enter deffrent numbers");
+// function dash(x) {
+//   let y = "";
+//   for (let i = 0; i < x.length; i++) {
+//     if (Number(x[i]) % 2 == 0 && Number(x[i + 1]) % 2 == 0) {
+//       y = y + x[i] + "-";
+//     } else {
+//       y = y + x[i];
+//     }
+//   }
+//   return y;
+// }
+// console.log(dash(x));
+
+// let z = "1";
+// z = z + "2";
+// console.log(z);
+
+// 24568
+// y=""
+// y= "" + 2 + -
+
+// let string = "hello";
+// console.log(string[0]);
+
+// Q5
+// function Agechecker(age) {
+//   if (age >= 18) {
+//     console.log("The user is Adult");
+//   } else {
+//     console.log("The user is Minor");
+//   }
+// }
+// Agechecker(5);
